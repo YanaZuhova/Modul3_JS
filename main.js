c2ea97c01c2fe10ebca03c54d301e95fb07a4cca
Iana <ziukhovaiana@gmail.com>
2023-08-12 15:42:25 +0300

    DZ.3.3

---
+++ main.js
@@ -0,0 +1,12 @@
+//названия статей
+let article1 = "JavaScript";
+let article2 = "Technology";
+
+console.log("article1 name is");
+console.log(article1);
+
+console.log("article2 name is");
+console.log(article2);
+
+let pojasnenie = "Почему я выбрал JS?"
+console.log(pojasnenie);
